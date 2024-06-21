74db88df320a88ba81bee183778d97c4a0a2dd22
Felipe Batista <felipe@taniquetil.com.ar>
2024-06-21 10:41:29 -0300

    Moví todo a la derecha :)

--- Terminal.py
+++ Terminal.py
@@ -6,7 +6,7 @@ TERMINAL = blessed.Terminal()
 
 class Clicker:
     def __init__(self):
-       self.x = 111
+       self.x = 61
        self.y = 32
        self.food = 0
 
@@ -14,21 +14,21 @@ class Clicker:
         print(TERMINAL.clear())
         print(TERMINAL.move_xy(self.x, self.y) + TERMINAL.orangered4("█") + TERMINAL.sienna("█"))
 
-        print(TERMINAL.move_xy(200, 30) + TERMINAL.limegreen(".\^/."))
-        print(TERMINAL.move_xy(197, 31) + TERMINAL.limegreen("'. |`|/| ." ))
-        print(TERMINAL.move_xy(197, 32) + TERMINAL.limegreen("'|\|\|'|/|" ))
-        print(TERMINAL.move_xy(195, 33) + TERMINAL.limegreen(".--'-\`|/-''--." ))
-        print(TERMINAL.move_xy(196, 34) + TERMINAL.limegreen("\`-._\|./.-'/" ))
-        print(TERMINAL.move_xy(197, 35) + TERMINAL.limegreen(">`-._|/.-'<" ))
-        print(TERMINAL.move_xy(196, 36) + TERMINAL.limegreen("'~|/~~|~~\|~'" ))
-        print(TERMINAL.move_xy(202, 37) + TERMINAL.limegreen("|"))
-        print(TERMINAL.move_xy(191, 28) + TERMINAL.bold(TERMINAL.limegreen("[SPACE] to byte the Leaf")))
+        print(TERMINAL.move_xy(150, 30) + TERMINAL.limegreen(".\^/."))
+        print(TERMINAL.move_xy(147, 31) + TERMINAL.limegreen("'. |`|/| ." ))
+        print(TERMINAL.move_xy(147, 32) + TERMINAL.limegreen("'|\|\|'|/|" ))
+        print(TERMINAL.move_xy(145, 33) + TERMINAL.limegreen(".--'-\`|/-''--." ))
+        print(TERMINAL.move_xy(146, 34) + TERMINAL.limegreen("\`-._\|./.-'/" ))
+        print(TERMINAL.move_xy(147, 35) + TERMINAL.limegreen(">`-._|/.-'<" ))
+        print(TERMINAL.move_xy(146, 36) + TERMINAL.limegreen("'~|/~~|~~\|~'" ))
+        print(TERMINAL.move_xy(152, 37) + TERMINAL.limegreen("|"))
+        print(TERMINAL.move_xy(141, 28) + TERMINAL.bold(TERMINAL.limegreen("[SPACE] to byte the Leaf")))
 
-        print(TERMINAL.move_xy(100, 33) + "       _/======\_" )
-        print(TERMINAL.move_xy(100, 34) + "     _/          \__" )
-        print(TERMINAL.move_xy(100, 35) + "   _/               \_" )
-        print(TERMINAL.move_xy(100, 36) + "  /                   \__")
-        print(TERMINAL.move_xy(100, 37) + " /                       \==============---------==========------===----------================" )
+        print(TERMINAL.move_xy(50, 33) + "       _/======\_" )
+        print(TERMINAL.move_xy(50, 34) + "     _/          \__" )
+        print(TERMINAL.move_xy(50, 35) + "   _/               \_" )
+        print(TERMINAL.move_xy(50, 36) + "  /                   \__")
+        print(TERMINAL.move_xy(50, 37) + " /                       \==============---------==========------===----------================" )
 
         if self.food > 0:
             print(TERMINAL.move_xy(191, 36) + TERMINAL.forestgreen("█") +
