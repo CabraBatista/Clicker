960b8d7c2fd7a2d7a84369493919ed92f8661785
Felipe Batista <felipe@taniquetil.com.ar>
2024-06-21 12:34:48 -0300

    La hormiga ahora cae automaticamente y solo la podes mover con A y D

--- Terminal.py
+++ Terminal.py
@@ -13,13 +13,13 @@ class Clicker:
        self.draw_base_hormiguero_x = int((TERMINAL.width - self.draws_size_x) / 2)
        self.draw_base_hormiguero_y = int((TERMINAL.height - self.draws_size_y) / 2)
 
-       self.x =  11
-       self.y = -5
+       self.ant_x =  11
+       self.ant_y = -5
        self.food = 0
 
     def draw(self):
         print(TERMINAL.clear())
-        print(TERMINAL.move_xy(self.x + self.draw_base_hormiguero_x, self.y + self.draw_base_hormiguero_y) + TERMINAL.orangered4("█") + TERMINAL.sienna("█"))
+        print(TERMINAL.move_xy(self.ant_x + self.draw_base_hormiguero_x, self.ant_y + self.draw_base_hormiguero_y) + TERMINAL.orangered4("█") + TERMINAL.sienna("█"))
 
         print(TERMINAL.move_xy(self.draw_base_hormiguero_x + 100, self.draw_base_hormiguero_y - 7) + TERMINAL.limegreen(".\^/."))
         print(TERMINAL.move_xy(self.draw_base_hormiguero_x + 97, self.draw_base_hormiguero_y - 6) + TERMINAL.limegreen("'. |`|/| ." ))
@@ -35,7 +35,7 @@ class Clicker:
         print(TERMINAL.move_xy(self.draw_base_hormiguero_x, self.draw_base_hormiguero_y - 3) + "     _/          \__" )
         print(TERMINAL.move_xy(self.draw_base_hormiguero_x, self.draw_base_hormiguero_y - 2) + "   _/               \_" )
         print(TERMINAL.move_xy(self.draw_base_hormiguero_x, self.draw_base_hormiguero_y - 1) + "  /                   \__")
-        print(TERMINAL.move_xy(self.draw_base_hormiguero_x, self.draw_base_hormiguero_y) + " /                       \==============---------==========------===----------================" )
+        print(TERMINAL.move_xy(self.draw_base_hormiguero_x, self.draw_base_hormiguero_y) +     " /                       \==============---------==========------===----------================" )
 
         if self.food > 0:
             print(TERMINAL.move_xy(self.draw_base_hormiguero_x + 92, self.draw_base_hormiguero_y - 1) + TERMINAL.forestgreen("█") +
@@ -51,19 +51,30 @@ class Clicker:
                 if val.lower() == " ":
                     self.food += 1
                 elif val.lower() == "d":
-                    self.x += 1
+                    if self.ant_x != 89:
+                        self.ant_x += 1
+                        self.walk()
                 elif val.lower() == "a":
-                    self.x -= 1
-                elif val.lower() == "s":
-                    self.y += 1
-                elif val.lower() == "w":
-                    self.y -= 1
+                    if self.ant_x != 10:
+                        self.ant_x -= 1
+                        self.walk()
 
                 self.draw_base_hormiguero_x = int((TERMINAL.width - self.draws_size_x) / 2)
                 self.draw_base_hormiguero_y = int((TERMINAL.height - self.draws_size_y) / 6 * 5 )
 
         print(TERMINAL.clear())
 
+    def walk(self):
+        if self.ant_x >=10 and self.ant_x <=16:
+            self.ant_y = -5
+        elif self.ant_x >= 17 and self.ant_x <= 19:
+            self.ant_y = -4
+        elif self.ant_x >= 20 and self.ant_x <= 21:
+            self.ant_y = -3
+        elif self.ant_x >= 22 and self.ant_x <= 24:
+            self.ant_y = -2
+        else:
+            self.ant_y = -1
 
 clicker = Clicker()
 clicker.loop()
