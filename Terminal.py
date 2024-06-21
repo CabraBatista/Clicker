55892f3a33a7e01c8b176939011caefc50a6cd33
Felipe Batista <felipe@taniquetil.com.ar>
2024-06-21 14:08:35 -0300

    Ahora tiras las hojas con E y cuando vayas tirando más hojitas se va mostrando

--- Terminal.py
+++ Terminal.py
@@ -143,6 +143,7 @@ class Clicker:
                 + str(self.load)
             )
 
+
     def loop(self):
         with TERMINAL.cbreak():
             val = ""
@@ -158,19 +159,19 @@ class Clicker:
                         self.ant_x += 1
                         self.walk()
                 elif val.lower() == "a":
-                    if self.ant_x != 10:
+                    if self.ant_x != 11:
                         self.ant_x -= 1
                         self.walk()
                 elif val.lower() == "e":
-                    self.load_food()
+                    if self.ant_x == 89:
+                        self.load_food()
+                    else:
+                        self.drop_food()
                 elif val.lower() == "r":
                     self.drop_food()
-                self.draw_base_hormiguero_x = int(
-                    (TERMINAL.width - self.draws_size_x) / 2
-                )
-                self.draw_base_hormiguero_y = int(
-                    (TERMINAL.height - self.draws_size_y) / 6 * 5
-                )
+
+                self.draw_base_hormiguero_x = int((TERMINAL.width - self.draws_size_x) / 2)
+                self.draw_base_hormiguero_y = int((TERMINAL.height - self.draws_size_y) / 6 * 5)
 
         print(TERMINAL.clear())
 
@@ -179,8 +180,9 @@ class Clicker:
 
     def can_load_food(self):
         return (
-            self.ant_x == 89
-            and self.ant_y == -1
+            #self.ant_x == 89
+            #and
+            self.ant_y == -1
             and self.load < self.max_load
             and self.food >= 1
         )
@@ -203,18 +205,43 @@ class Clicker:
     def draw_points(self):
         print(
             TERMINAL.move_xy(
-                self.draw_base_hormiguero_x + 5, self.draw_base_hormiguero_y - 20
+                self.draw_base_hormiguero_x + 6, self.draw_base_hormiguero_y - 8
             )
-            + f"POINTS: {self.points}"
+            + TERMINAL.goldenrod1(TERMINAL.bold(f"POINTS: {self.points}"))
         )
-        if self.points > 0:
-            if self.points < 100:
+        if self.points < 100:
+            if self.points > 0:   
                 print(
                     TERMINAL.move_xy(
                         self.draw_base_hormiguero_x + 5, self.draw_base_hormiguero_y
                     )
                     + TERMINAL.forestgreen("█")
                 )
+            if self.points >= 5:
+                print(TERMINAL.move_xy(self.draw_base_hormiguero_x + 6, self.draw_base_hormiguero_y) +
+                       TERMINAL.forestgreen("█"))
+            if self.points >= 10:
+                print(TERMINAL.move_xy(self.draw_base_hormiguero_x + 7, self.draw_base_hormiguero_y) +
+                       TERMINAL.forestgreen("█"))
+            if self.points >= 15:
+                print(TERMINAL.move_xy(self.draw_base_hormiguero_x + 5, self.draw_base_hormiguero_y - 1) +
+                       TERMINAL.forestgreen("█"))
+            if self.points >= 20:
+                print(TERMINAL.move_xy(self.draw_base_hormiguero_x + 6, self.draw_base_hormiguero_y - 1) +
+                       TERMINAL.forestgreen("█"))
+            if self.points >= 30:
+                print(TERMINAL.move_xy(self.draw_base_hormiguero_x + 8, self.draw_base_hormiguero_y) +
+                       TERMINAL.forestgreen("█"))
+            if self.points >= 40:
+                print(TERMINAL.move_xy(self.draw_base_hormiguero_x + 9, self.draw_base_hormiguero_y) +
+                       TERMINAL.forestgreen("█"))
+            if self.points >= 45:
+                print(TERMINAL.move_xy(self.draw_base_hormiguero_x + 7, self.draw_base_hormiguero_y - 1) +
+                       TERMINAL.forestgreen("█"))
+                
+                       
+        
+
 
     def walk(self):
         if self.ant_x >= 10 and self.ant_x <= 16:
